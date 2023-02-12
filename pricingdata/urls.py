51002e6da225db9e0cc542152d7362003d48fbdd
ShafayetZim <shafayet.zim@northsouth.edu>
2023-02-12 18:41:50 +0600

    urls of pricing app

---
+++ pricingdata/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.dashboard, name="dashboard"),
+    path('bg_pricing', views.BgPricingList.as_view(), name="pricing-bg-list"),
+    path('update_pricing_bg/<int:pk>', views.BgPricingUpdateView.as_view(), name='pricing-bg-update'),
+    path('pricing', views.PricingList.as_view(), name="pricing-list"),
+    path('update_pricing/<int:pk>', views.PricingUpdateView.as_view(), name='pricing-update'),
+    path('new_pricing', views.new_pricing, name="new-pricing"),
+    path('delete_pricing/<int:id>', views.pricing_delete, name="delete-pricing"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
