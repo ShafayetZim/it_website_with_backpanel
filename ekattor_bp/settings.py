006735aa6a32a321339d8948d77da01a6a19c568
ShafayetZim <shafayet.zim@northsouth.edu>
2023-02-17 17:59:10 +0600

    authentication added

--- ekattor_bp/settings.py
+++ ekattor_bp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'contactdata',
     'portfoliodata',
     'webapp',
+    'authentication',
     'crispy_forms',
     'ckeditor',
 ]
@@ -136,3 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
